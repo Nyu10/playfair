917d8c4a5d3c96e3e819b97458bf7f853b9749ef
Nehemiah Yu <nehemiahyu1@gmail.com>
2020-10-10 17:01:38 -0400

    Did 1st Part

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main{
     public static void main(String[]args){
 	String Encode = "WHITEHAT PLAYFIREXMBCDGHKNOQSTUVWZ";
-
+	String message =Encode.substring(0,Encode.indexOf(' '));
+	System.out.println(addX("abooksoop"));
     }
+	
+	
+	public static String addX(String message){
+		for (int i=0;i<message.length()-1;i+=2){
+			String pair = message.substring(i,i+2);
+			if (pair.substring(0,1).equals(pair.substring(1))){
+				message = message.substring(0,i+1) + "x" + message.substring(i+1);
+			}
+		}
+		if (message.length()%2!=0){
+			message+="x";
+		}
+		return message;
+	}
 }
