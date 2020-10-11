4ad5413a2cef2c41a0ae1439b68322c876eb2ada
Nehemiah Yu <nehemiahyu1@gmail.com>
2020-10-11 00:27:38 -0400

    Encoding almost fully works except for 2 letters

--- Main.java
+++ Main.java
@@ -11,18 +11,35 @@ public class Main{
 	String key = Input.substring(EorD.length()+2+message.length());
 	//playfair key in Two-Dimensional Array
 	String [][] dataStructure = makeStructure(key);
+	String answer="";
 	if (EorD.equals("encode")){
-		String answer = encode(ciphertext,datastructure);
+		answer = encode(ciphertext,dataStructure);
 	}
 	else {
-		String answer = decode(ciphertext,dataStructure);
+		answer = decode(ciphertext,dataStructure);
 	}
 	System.out.println(EorD+"d Text"+answer);
     }
-	public String encode(String ciphertext, String [][] datastructure){
+	public static String encode(String ciphertext, String [][] dataStructure){
+		String answer="";
 		for (int i=0;i<ciphertext.length();i+=2){
-			String indices = returnIndices(ciphertext.substring(i,i+2);
+			String indices = returnIndices(ciphertext.substring(i,i+2),dataStructure);
+			//Vertical, Horitzontal, Regular
+			String VHorR = analyze(indices);
+			if (VHorR.equals("vertical")){
+				answer+=verticalEncode(ciphertext.substring(i,i+2),dataStructure);
+			}
+			else if (VHorR.equals("horizontal")){
+				answer+=horizontalEncode(ciphertext.substring(i,i+2),dataStructure);
+			}
+			else if (VHorR.equals("regular")){
+				answer+=regularEncode(ciphertext.substring(i,i+2),dataStructure);
+			}
 		}
+		return answer;
+	}
+	public static String decode(String ciphertext, String [][]datastructure){
+		return "not done yet";
 	}
 	public static String addX(String message){
 		for (int i=0;i<message.length()-1;i+=2){
@@ -47,7 +64,7 @@ public class Main{
 		}
 		return keyStructure;
 	}
-	public String analyis(String indices){
+	public static String analyze(String indices){
 		String r1,c1,r2,c2;
 		r1=indices.substring(0,1);
 		c1=indices.substring(1,2);
@@ -57,7 +74,7 @@ public class Main{
 		else if (c1==c2) return "vertical";
 		else return "regular";
 	}
-	public String returnIndices(String letterPair, String [][] key){
+	public static String returnIndices(String letterPair, String [][] key){
 	//r1 c1 = row column indices for first letterPair
 	int r1=0,c1=0,r2=0,c2=0;
 		for (int i=0;i<5;i++){
@@ -74,7 +91,7 @@ public class Main{
 		}
 	return "" +r1+c1+r2+c2;
 	}
-	public String verticalEncode(String letterPair,String [][]key){
+	public static String verticalEncode(String letterPair,String [][]key){
 		String indices= returnIndices(letterPair,key);
 		String newLetterPair = "";
 		//checking for edge case
@@ -94,7 +111,7 @@ public class Main{
 		newLetterPair+=key[Integer.parseInt(indices.substring(2,3))][Integer.parseInt(indices.substring(3,4))];
 		return newLetterPair;
 	}
-	public String horizontalEncode(String letterPair,String [][]key){
+	public static String horizontalEncode(String letterPair,String [][]key){
 		String indices= returnIndices(letterPair,key);
 		String newLetterPair = "";
 		//checking for edge case
@@ -114,7 +131,7 @@ public class Main{
 		newLetterPair+=key[Integer.parseInt(indices.substring(2,3))][Integer.parseInt(indices.substring(3,4))];
 		return newLetterPair;
 	}
-	public String regularEncode(String letterPair,String [][]key){
+	public static String regularEncode(String letterPair,String [][]key){
 		String indices= returnIndices(letterPair,key);
 		String newLetterPair = "";
 		int r1=Integer.parseInt(indices.substring(0,1));
